e3ac8aa5cd441627a8fff8c3dcae0f730fadf474
Lara Costa <larasmila64@gmail.com>
2023-05-22 13:41:48 -0300

    feat: add global style to modal

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -9,10 +10,20 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-    return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-        <h2>Cadastrar Transacao</h2>
-      </Modal>
-    )
- 
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar Transação</h2>
+        <input type="text" placeholder="Titulo" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
 }
