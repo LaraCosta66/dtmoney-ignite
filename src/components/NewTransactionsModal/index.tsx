2102acd4c7b7122e43972ca81931b4692236aa36
Lara Costa <larasmila64@gmail.com>
2023-05-22 14:17:39 -0300

    feat: add income and outcome buttons

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
-
+import { Container, TransactionTypeContainer } from "./styles";
+import closeImg from "../../assets/close.svg";
+import incomeImg from  "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg"
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -17,10 +19,28 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button type="button" onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
       <Container>
         <h2>Cadastrar Transação</h2>
         <input type="text" placeholder="Titulo" />
         <input type="number" placeholder="Valor" />
+        <TransactionTypeContainer>
+        <button 
+        type="button"
+
+        >
+            <img src={incomeImg} alt="entrada"/>
+            <span>Entrada</span>
+        </button>
+        <button type="button">
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saida</span>
+        </button>
+
+
+        </TransactionTypeContainer>
         <input type="text" placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
