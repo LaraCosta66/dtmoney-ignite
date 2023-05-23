b16b66e1c7ac4b168d806dfb1d6286c7923c3827
Lara Costa <larasmila64@gmail.com>
2023-05-23 14:02:55 -0300

    feat: add createTransaction function to Transactions Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import Modal from "react-modal";
 import { NewTransactionModal } from "./components/NewTransactionsModal";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
@@ -18,7 +18,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider >
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -26,6 +26,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
---
+++ src/TransactionsContext.tsx
@@ -0,0 +1,42 @@
+import { ReactNode, createContext, useEffect, useState} from "react";
+import { api } from "./services/api";
+
+
+
+interface Transaction {
+    id: number;
+    title: string;
+    type: string;
+    category: string;
+    amount: number;
+    createdAt: string;
+  }
+interface TransactionsProviderProps{
+    children: ReactNode;
+}
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+ interface TransactionsContextData{
+    transactions: Transaction[];
+    createTransaction:(Transaction:TransactionInput) => void
+ }
+export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
+
+export function TransactionsProvider({children}:TransactionsProviderProps){
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    useEffect(() => {
+      api.get("transactions")
+        .then(response => setTransactions(response.data.transactions));
+    }, []);
+
+    function createTransaction(transaction:TransactionInput){
+        
+    api.post("/transactions", transaction);
+    }
+
+
+    return(
+       < TransactionsContext.Provider value={{transactions, createTransaction}}>
+       {children}
+       </TransactionsContext.Provider>
+    )
+}
